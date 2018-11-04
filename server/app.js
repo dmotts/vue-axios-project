b4cf5cf5a50be96cf07e0147e70392b6496ccb81
Daley <daley.mottley@hotmail.com>
2018-11-04 06:32:15 +0000

    Config server. Modify .gitignore. Install cors

--- server/app.js
+++ server/app.js
@@ -5,11 +5,13 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var userRoute = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var cors = require('cors');
 var mongoose = require('mongoose');
 var mongoDB = require('./database_config');
+var serverUrl = require('../server-url');
 
 var app = express();
 
@@ -29,21 +31,15 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// Set to only accept request from Vue frontend client address
+app.use(cors({credentials: true, origin: serverUrl + ':8081'}));
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.use('/', routes);
+app.use('/users', userRoute);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET users listing.
- */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
-};
+router.post('/', function(req, res, next) {
+    res.send('it works');
+});
+
+module.exports = router;
