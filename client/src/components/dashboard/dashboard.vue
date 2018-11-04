91d927a18dcd5b35ba2b82ddac1b396a03b0929b
Daley <daley.mottley@hotmail.com>
2018-11-04 14:32:52 +0000

    Extract data from users get request

--- client/src/components/dashboard/dashboard.vue
+++ client/src/components/dashboard/dashboard.vue
@@ -2,6 +2,7 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }}</p>
   </div>
 </template>
 
@@ -9,9 +10,25 @@
   import axios from 'axios';
   import serverUrl from '../../../../server-url'
   export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
     created() {
       axios.get(`${serverUrl}/users`)
-        .then(res => console.log(res))
+        .then(res => {
+          console.log(res);
+          const data = res.data.users
+          const users = []
+          for (let key in data) {
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          } 
+          console.log(users);
+          this.email = users[0].email;
+        })
         .catch(error => console.log(error))
     }
   }
